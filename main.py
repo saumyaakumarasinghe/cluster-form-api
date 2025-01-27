6679a5af95a8211c904c425a4d480a658d4c9375
Saumya Kumarasinghe <saumya.intern.emg@gmail.com>
2025-01-28 01:13:39 +0530

    refactor: remove manage and used main instead

--- main.py
+++ main.py
@@ -1,40 +1,9 @@
-from googleapiclient.discovery import build
-from google.oauth2 import service_account
+from flask.cli import FlaskGroup
+from src import create_app
 
-SERVICE_ACCOUNT_FILE = "keys.json"
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]  # read only readonly
+app = create_app()
 
-creds = None
-creds = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES
-)
+cli = FlaskGroup(create_app=create_app)
 
-# The ID spreadsheet
-SAMPLE_SPREADSHEET_ID = "1LbxrSc4axJx-gUjjzT-g8e_W9Y-kk7MtdlDgsExZOYk"
-
-service = build("sheets", "v4", credentials=creds)
-
-# Call the Sheets API
-sheet = service.spreadsheets()
-result = (
-    sheet.values()
-    .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range="Student Responses!A1:J63")
-    .execute()
-)
-values = result.get("values", [])
-
-# aoa = [["saumya", 1000], ["saumya2", 2000], ["saumya3", 3000]]
-
-# Write in the sheet
-request = (
-    sheet.values()
-    .update(
-        spreadsheetId=SAMPLE_SPREADSHEET_ID,
-        range="Sheet1!A1",
-        valueInputOption="USER_ENTERED",
-        body={"values": values},
-    )
-    .execute()
-)
-
-print(request)
+if __name__ == "__main__":
+    cli()

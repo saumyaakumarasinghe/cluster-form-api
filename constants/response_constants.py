e01b2ef06c6ec97cfc1c10b9edbc49a7ba0357ff
Saumya Kumarasinghe <94886034+saumyaakumarasinghe@users.noreply.github.com>
2025-03-22 16:29:56 +0530

    chore: rename files and requirements

--- constants/response_constants.py
+++ constants/response_constants.py
@@ -1,3 +1,4 @@
 EMPTY_LINK = "Link is empty"
-INVALID_LINK = "Invalid link format"
+INVALID_LINK = "Invalid spreadsheet link format"
 DATA_NOT_FOUND = "No data found in the specified range"
+ACCURACY_REQUIRED = "Accuracy required"
